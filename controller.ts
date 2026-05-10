541c49b3540a031a10161633e89272c0a60f5499
Gabriel-LG <373107+Gabriel-LG@users.noreply.github.com>
2026-05-10 12:03:02 +0200

    Add teacher tool proof of concept for muting

--- controller.ts
+++ controller.ts
@@ -41,3 +41,60 @@ namespace controller {
         started = true;
     }
 }
+
+//% weight=100 color=#EE7202 icon="\uf0a1"
+//% block="BattleBot Teacher"
+namespace teacher {
+
+    class State
+    {
+        constructor(){}
+        soundEnabled: boolean = true;
+        motorsEnabled: boolean = true;
+        victory: boolean = false;
+    }
+
+
+    let state: {[id: number]: State} = {};
+
+    function getState(id: number) : State
+    {
+        if (state[id] == undefined) state[id] = new State;
+        return state[id];
+    }
+
+
+
+    //% block
+    export function enableSound(id: number, enable: boolean): void {
+        getState(id).soundEnabled = enable;
+    }
+
+    //% block
+    export function enableMotors(id: number, enable: boolean): void {
+        getState(id).motorsEnabled = enable;
+    }
+
+    //% block
+    export function setVictory(id: number, value: boolean): void {
+        getState(id).victory = value;
+    }
+
+    //% block
+    export function transmitState(id: number): void {
+        radio.setGroup(0);
+        radio.setFrequencyBand(id * 5);
+
+        let state: State = getState(id);
+
+        let flags: number = 0;
+        flags |= state.soundEnabled ? 0x00 : 0x01;
+        flags |= state.motorsEnabled ? 0x00 : 0x01;
+        flags |= state.victory ? 0x80 : 0x00;
+
+        let buffer: Buffer = pins.createBuffer(2);
+        buffer.setUint8(0, 0x54 /*'T'*/); // 'T' for teacher update
+        buffer.setNumber(NumberFormat.Int8BE, 1, flags);
+        radio.sendBuffer(buffer);
+    }
+}
--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+serial.onDataReceived(serial.delimiters(Delimiters.CarriageReturn), function () {
+    line = serial.readUntil(serial.delimiters(Delimiters.CarriageReturn))
+    if (line == "mute") {
+        for (let index = 0; index <= 15; index++) {
+            teacher.enableSound(index, false)
+            teacher.transmitState(index)
+        }
+        basic.showIcon(IconNames.No)
+    } else if (line == "unmute") {
+        for (let index = 0; index <= 15; index++) {
+            teacher.enableSound(index, true)
+            teacher.transmitState(index)
+        }
+        basic.showIcon(IconNames.Yes)
+    }
+})
+let line = ""
+serial.redirectToUSB()
 let channel = 0
 controller.initController(channel)
 basic.showNumber(channel)
